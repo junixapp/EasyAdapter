d7f297da8236b1cafed642a7edd711583754b55a
li-xiaojun <lxj@lxjdeiMac.local>
2018-12-05 08:50:23 +0800

    make HeaderAndFooterWrapper package-public

--- easy-adapter/src/main/java/com/lxj/easyadapter/HeaderAndFooterWrapper.java
+++ easy-adapter/src/main/java/com/lxj/easyadapter/HeaderAndFooterWrapper.java
@@ -13,7 +13,7 @@ import android.view.ViewGroup;
 /**
  * Created by zhy on 16/6/23.
  */
- class HeaderAndFooterWrapper<T> extends RecyclerView.Adapter<RecyclerView.ViewHolder>
+public class HeaderAndFooterWrapper<T> extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 {
     private static final int BASE_ITEM_TYPE_HEADER = 100000;
     private static final int BASE_ITEM_TYPE_FOOTER = 200000;
